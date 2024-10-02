8ac6385e7d139d2570d92ff3a9a7745efa118394
nicholves <nicholves@hotmail.com>
2024-10-01 23:40:47 -0400

    R-CODE-03-RESP-03

--- A1-Veselskiy-Nicholas-101192011/src/main/java/Window/Window.java
+++ A1-Veselskiy-Nicholas-101192011/src/main/java/Window/Window.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 
 public class Window {
     public void promptToEndTurn(Scanner input, PrintWriter output) {
+        output.println("Your Turn is Over Press <Enter>");
+        input.nextLine();
 
+        for (int i = 0; i < 100; i++) {
+            output.println();
+        }
     }
 }
